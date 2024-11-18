e84f78ac5a00674f08a694044896194750e89277
Leyla Becker <git@jan-leila.com>
2024-11-17 19:37:13 -0600

    updated site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://27th-colfax.github.io/',
 	integrations: [mdx(), sitemap()],
 });
