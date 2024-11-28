f96bc51bdcf32be4333f9cd687cf9bc1c00a2ab3
Leyla Becker <git@jan-leila.com>
2024-11-27 19:51:33 -0600

    added events to RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,31 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { getAllEvents } from '../components/calendar';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const blog = await getCollection('blog');
+
+	const blogItems = blog.map((post) => ({
+		...post.data,
+		link: `/blog/${post.slug}/`,
+	}))
+
+	const events = await getAllEvents()
+
+	const eventsItems = events.map((event) => ({
+		link: `/event/${event.params.date}/${event.params.slug}`,
+		pubDate: event.props.interval.start.startOf('day').toISO(),
+		title: event.props.title,
+		description: event.props.description,
+	}))
+
+	const items = [...blogItems, ...eventsItems]
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items,
 	});
 }
